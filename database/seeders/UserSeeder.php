79f33f91d74ed36ade0c05d1f262d39e9412c47f
David Anthony Contreras Cerron <74465364@continental.edu.pe>
2025-07-05 22:19:35 -0500

    fix: evitar duplicados al ejecutar seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,18 +10,22 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        // Usuario administrador manual
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('123'),
-        ]);
+        // Admin
+        if (!User::where('email', 'admin@gmail.com')->exists()) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123'),
+            ]);
+        }
 
-        // Usuario de prueba manual
-        User::create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('test123'), // Asegúrate de ponerle clave
-        ]);
+        // Usuario de prueba
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'password' => Hash::make('test123'),
+            ]);
+        }
     }
 }
